e4c39b4afb184b9a87697f21e4f53dcb9831d130
Igor Gomes <iguinhogms@gmail.com>
2023-02-02 01:15:31 -0300

    Criar tarefas finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,33 @@ import {
 } from './styles/stylesModal'
 
 function App() {
+  const [showAlert, setShowAlert] = useState(false)
   const [showElement, setShowElement] = useState(false);
-  const [list, setList] = useState("")
+  const [task, setTask] = useState("")
+  const [list, setList] = useState([])
 
   const ShowElement = () => setShowElement(true);
   const HideElement = () => setShowElement(false);
 
+  function handleChangeInput(e) {
+    const inputTask = e.target.value
+    setTask(inputTask)
+  }
+
+  function handleItemToList(e) {
+    e.preventDefault();
+
+    if (!task) {
+      return setShowAlert(true);
+    }
+
+    setList([...list, task])
+
+    setTask("")
+    setShowAlert(false)
+    HideElement()
+  }
+  
   return (
     <Container>
       <Display>
@@ -26,11 +47,18 @@ function App() {
           
           {showElement ? 
                     <Modal>
-                    <form></form>
-                    <button onClick={HideElement}> + </button>
+                      <form onSubmit={handleItemToList}>
+                        <button onClick={HideElement}> - </button>
+                        <input type="text" placeholder="Digite a sua tarefa..." onChange={handleChangeInput} value={task}/>
+                        {showAlert ? <p>Digite a tarefa antes de adiciona-la</p> : null}
+                        <button type="submit"> Enviar </button>
+                      </form>
                   </Modal>
           : null}
 
+          <ul>
+            {list.map((item, index) => (<li key={index}>{item}</li>))}
+          </ul>
         </Body>
       </Display>
     </Container>
